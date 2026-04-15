e3cdd8766289946a92926b1a321c68a5ac99090e
Amr Elabbasy <amrelabbasy2003@gmail.com.com>
2026-04-15 18:16:57 +0200

    Test

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_hello():
+    client = app.test_client()
+    response = client.get("/how are you")
+    assert response.status_code == 200
